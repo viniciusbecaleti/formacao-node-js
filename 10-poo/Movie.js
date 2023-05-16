e9eaa7408800df080ca1530a7016045a3d5f4762
Vinicius Becaleti <vinicius@becaleti.com>
2023-05-16 19:50:50 -0300

    Início do módulo sobre OOP

---
+++ 10-poo/Movie.js
@@ -0,0 +1,25 @@
+class Movie {
+  constructor() {
+    this.name = ""
+    this.genre = ""
+    this.sinopse = ""
+    this.releaseDate = ""
+    this.director = ""
+    this.budget = ""
+    this.distributedBy = ""
+    this.boxOffice = ""
+    this.cast = []
+  }
+
+  play() { /* play */ }
+  
+  pause() { /* pause */ }
+
+  stop() { /* stop */ }
+
+  rewind() { /* rewind */ }
+
+  skip() { /* skip */ }
+
+  forward() { /* forward */ }
+}
